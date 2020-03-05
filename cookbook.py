dc369a47aac49369fb08ee41d8ed0023c99110bb
glyph <mail@strangegly.ph>
2020-03-05 22:11:01 +0100

    Initial commit

---
+++ cookbook.py
@@ -0,0 +1,189 @@
+import os
+from typing import List, Dict, Union, Optional
+import os.path as ospath
+from ruamel.yaml import YAML, yaml_object
+
+yaml = YAML()
+yaml.indent(mapping=2, sequence=4, offset=2)
+
+
+class LoadException(Exception):
+    def __init__(self, msg: str):
+        super(LoadException, self).__init__(msg)
+
+
+class Ingredient:
+    def __init__(self, ingredient: str, amount: int = None, unit: str = None):
+        self.ingredient: str = ingredient
+        self.amount: Optional[int] = amount
+        self.unit: Optional[str] = unit
+
+
+class RecipeStep:
+    def __init__(self, instructions: str,
+                 ingredients: List[Dict[str,str]] = None,
+                 internal_ingredients: List[str] = None,
+                 yields: Union[str, List[str]] = None):
+        self.instructions: str = instructions
+
+        self.ingredients: List[Ingredient] = []
+        if ingredients is not None:
+            for i, ingredient in enumerate(ingredients):
+                try:
+                    self.ingredients.append(Ingredient(**ingredient))
+                except TypeError as e:
+                    if e.args and 'required positional argument' in e.args[0]:
+                        field = e.args[0].split('\'')[1]
+                        raise LoadException(f"ingredient {i} is missing a required field: '{field}'")
+                    else:
+                        raise e
+
+        self.internal_ingredients: List[str] = []
+        if internal_ingredients is not None:
+            self.internal_ingredients = internal_ingredients
+
+        self.yields: List[str] = []
+        if yields is not None:
+            if type(yields) == list:
+                self.yields = yields
+            else:
+                self.yields = [yields]
+
+class Recipe:
+    def __init__(self, id: str, lang: str, name: str, serves: int,
+                 note: str = None,
+                 tags: Union[str, List[str]] = None,
+                 prep: List[Dict[str, str]] = None,
+                 mis_en_place: List[Dict[str, str]] = None,
+                 cooking: List[Dict[str, str]] = None,
+                 passive_cooking: List[Dict[str, str]] = None):
+        self.id: str = id
+        self.lang: str = lang
+        self.name: str = name
+        self.serves: int = serves
+        self.note: Optional[str] = note
+
+        self.tags: List[str] = []
+        if tags is not None:
+            if type(tags) == str:
+                tags = tags.split()
+            for tag in tags:
+                self.tags.append(tag.lower().strip())
+
+        self.prep: List[RecipeStep] = []
+        if prep is not None:
+            for i, step in enumerate(prep):
+                try:
+                    self.prep.append(RecipeStep(**step))
+                except TypeError as e:
+                    if e.args and 'required positional argument' in e.args[0]:
+                        field = e.args[0].split('\'')[1]
+                        raise LoadException(f"Recipe {id}.{lang}: prep step {i} is missing a required field: '{field}'")
+                    else:
+                        raise e
+                except LoadException as e:
+                    e.args[0] = f"Recipe {id}.{lang}: prep step {i}: {e.args[0]}"
+
+        self.mis_en_place: List[RecipeStep] = []
+        if mis_en_place is not None:
+            for i, step in enumerate(mis_en_place):
+                try:
+                    self.mis_en_place.append(RecipeStep(**step))
+                except TypeError as e:
+                    if e.args and 'required positional argument' in e.args[0]:
+                        field = e.args[0].split('\'')[1]
+                        raise LoadException(f"Recipe {id}.{lang}: mis-en-place step {i} is missing a required field: '{field}'")
+                    else:
+                        raise e
+                except LoadException as e:
+                    e.args[0] = f"Recipe {id}.{lang}: mis-en-place step {i}: {e.args[0]}"
+
+        self.cooking: List[RecipeStep] = []
+        if cooking is not None:
+            for i, step in enumerate(cooking):
+                try:
+                    self.cooking.append(RecipeStep(**step))
+                except TypeError as e:
+                    if e.args and 'required positional argument' in e.args[0]:
+                        field = e.args[0].split('\'')[1]
+                        raise LoadException(f"Recipe {id}.{lang}: cooking step {i} is missing a required field: '{field}'")
+                    else:
+                        raise e
+                except LoadException as e:
+                    e.args[0] = f"Recipe {id}.{lang}: cooking step {i}: {e.args[0]}"
+
+        self.passive_cooking: List[RecipeStep] = []
+        if passive_cooking is not None:
+            for i, step in enumerate(passive_cooking):
+                try:
+                    self.passive_cooking.append(RecipeStep(**step))
+                except TypeError as e:
+                    if e.args and 'required positional argument' in e.args[0]:
+                        field = e.args[0].split('\'')[1]
+                        raise LoadException(f"Recipe {id}.{lang}: passive step {i} is missing a required field: '{field}'")
+                    else:
+                        raise e
+                except LoadException as e:
+                    e.args[0] = f"Recipe {id}.{lang}: passive step {i}: {e.args[0]}"
+
+    @staticmethod
+    def split_path(path: str) -> (str, str):
+        _, fname = ospath.split(path)
+        root, ext = ospath.splitext(fname)
+        if ext != ".yml" and ext != ".yaml":
+            raise LoadException(f"{path}: Expect yaml file, got {ext}")
+
+        recipe_id, lang = ospath.splitext(root)
+        lang = lang[1:]
+        return recipe_id, lang
+
+    @staticmethod
+    def load(path: str) -> "Recipe":
+
+        recipe_id, lang = Recipe.split_path(path)
+        try:
+            with open(path, 'r') as f:
+                recipe = Recipe(recipe_id, lang, **yaml.load(f))
+                return recipe
+        except TypeError as e:
+            if e.args and 'required positional argument' in e.args[0]:
+                field = e.args[0].split('\'')[1]
+                raise LoadException(f"Recipe {recipe_id}.{lang} is missing a required field: '{field}'")
+            else:
+                raise e
+
+
+class RecipeTranslations:
+    def __init__(self):
+        self.translations: Dict[str, Recipe] = {}
+
+
+class Cookbook:
+    def __init__(self):
+        self.by_id: Dict[str, RecipeTranslations] = {}
+        self.by_language: Dict[str, List[Recipe]] = {}
+
+    @staticmethod
+    def load_folder(path: str) -> ("Cookbook", List[LoadException]):
+        book = Cookbook()
+        errors = []
+        for root, _, files in os.walk(path):
+            for file in files:
+                try:
+                    if ospath.splitext(file)[1] != ".yml" and ospath.splitext(file)[1] != ".yaml":
+                        continue
+
+                    fpath = ospath.join(root, file)
+                    recipe = Recipe.load(fpath)
+
+                    if recipe.id not in book.by_id:
+                        book.by_id[recipe.id] = RecipeTranslations()
+                    book.by_id[recipe.id].translations[recipe.lang] = recipe
+
+                    if recipe.lang not in book.by_language:
+                        book.by_language[recipe.lang] = []
+                    book.by_language[recipe.lang].append(recipe)
+                except LoadException as e:
+                    errors.append(e)
+
+        return book, errors
---
+++ main.py
@@ -0,0 +1,85 @@
+import flask
+from flask import Flask, request
+import os
+
+from cookbook import Cookbook
+
+
+app = Flask(__name__)
+app.config.from_json("config.json")
+
+book = Cookbook()
+
+
+def lang():
+    if request and request.cookies and "lang" in request.cookies:
+        if request.cookies[lang] in book.by_language:
+            return request.cookies.get("lang")
+        else:
+            request.cookies["lang"] = app.config["defaultlang"]
+
+    return app.config["defaultlang"]
+
+
+@app.route('/')
+def index():
+    resp = flask.render_template('index.jinja2', langs=book.by_language.keys())
+    if request.cookies.get("lang") not in book.by_language:
+        resp.set_cookie("lang", app.config["defaultlang"])
+    if request.args.get("lang") in book.by_language:
+        resp.set_cookie("lang", app.config["defaultlang"])
+    return resp
+
+
+@app.route('/search')
+def search():
+    query = request.args["query"]
+    if not query:
+        return flask.redirect('/')
+    norm_query = query.lower()
+    words = norm_query.split()
+
+    results = set()
+    for recipe_trans in book.by_id.values():
+        for recipe in recipe_trans.translations.values():
+            for tag in recipe.tags:
+                if tag in words:
+                    results.add(recipe)
+            if norm_query in recipe.name:
+                results.add(recipe)
+
+    for recipe in results:
+        print(f"{recipe.name} [{recipe.lang}]")
+
+    if not results:
+        flask.flash("No result found :(")
+        flask.redirect('/')
+
+    resp = flask.make_response(flask.render_template('listing.jinja2', langs=book.by_language.keys(), active_lang=lang()))
+    return resp
+
+
+if __name__ == "__main__":
+    extra_dirs = ["templates", "static"]
+    extra_files = extra_dirs[:]
+    for extra_dir in extra_dirs:
+        for dirname, dirs, files in os.walk(extra_dir):
+            for filename in files:
+                filename = os.path.join(dirname, filename)
+                if os.path.isfile(filename):
+                    extra_files.append(filename)
+
+    book, errors = Cookbook.load_folder(app.config["COOKBOOK_LOCATION"])
+    print(f"Cookbook: {len(book.by_id)} recipes loaded")
+    for language, collection in book.by_language.items():
+        print(f"- {language}: {len(collection)}")
+    if errors:
+        print("The following errors occured while trying to load recipes:")
+        for error in errors:
+            print(f"- {error.args[0]}")
+
+    if "defaultlang" not in app.config:
+        app.config["defaultlang"] = max(book.by_language.keys(), key=(lambda k: len(book.by_language[k])))
+
+    app.run(extra_files=extra_files)
+    app.run()
